def98553d26114ae2b851d02078b24e202bb12a9
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-09-03 15:19:12 -0300

    fix: Resolve text formatting issue

--- resources/documentation.py
+++ resources/documentation.py
@@ -4,12 +4,12 @@ from flask_openapi3 import Tag
 from app import app
 
 tag_documentation = Tag(
-    name = "Documentation",
-    description = "Documentation selection: Swagger, Redoc or RapiDoc.",
+    name="Documentation",
+    description="Documentation selection: Swagger, Redoc or RapiDoc.",
 )
 
 
-@app.get("/", tags = [tag_documentation])
+@app.get("/", tags=[tag_documentation])
 def documentation_route():
     """
     Redirects to the /openapi route,\
