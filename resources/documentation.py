278a9e44ed63eb277224550b3f1828e83003815d
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-09-03 15:08:41 -0300

    feat: Documentation route created

---
+++ resources/documentation.py
@@ -0,0 +1,18 @@
+from flask import redirect
+from flask_openapi3 import Tag
+
+from app import app
+
+tag_documentation = Tag(
+    name = "Documentation",
+    description = "Documentation selection: Swagger, Redoc or RapiDoc.",
+)
+
+
+@app.get("/", tags = [tag_documentation])
+def documentation_route():
+    """
+    Redirects to the /openapi route,\
+    a screen that allows choosing the documentation style.
+    """
+    return redirect("/openapi")
