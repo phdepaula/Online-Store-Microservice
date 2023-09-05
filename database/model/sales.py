832956e7024f29f879d47df8cb5cdbdabf58ecdf
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-09-04 22:11:12 -0300

    fix: Fixing parameters

--- database/model/sales.py
+++ database/model/sales.py
@@ -12,12 +12,12 @@ class Sales(BASE):
     __tablename__ = "sales"
 
     sales_id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(30), unique=True)
+    name = Column(String(30))
     quantity = Column(Integer)
     value = Column(Float)
     sale_status = Column(String(10), default="open")
     sale_date = Column(String(10), default=datetime.today())
-    zip_code = Column(String(9))
+    zip_code = Column(String(15))
     country = Column(String(50))
     city = Column(String(50))
     state = Column(String(50))
@@ -29,7 +29,7 @@ class Sales(BASE):
         name: str,
         quantity: int,
         value: float,
-        zip_cod: str,
+        zip_code: str,
         country: str,
         city: str,
         state: str,
@@ -39,7 +39,7 @@ class Sales(BASE):
         self.name = name
         self.quantity = quantity
         self.value = value
-        self.zip_cod = zip_cod
+        self.zip_code = zip_code
         self.country = country
         self.city = city
         self.state = state
