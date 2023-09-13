ff59c4ba7b1591f6b2cbcfff9f2f9e9b0deb45a2
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-09-13 11:20:45 -0300

    feat: Adding log

--- resources/products.py
+++ resources/products.py
@@ -1,13 +1,13 @@
 from urllib.parse import unquote
 from flask_openapi3 import Tag
 
-from app import app, database
+from app import app, database, log
 from database.model.product import Product
 from database.model.sales import Sales
 from schemas.products import (
     AddProductSchema,
-    ProductNameSchema,
     MessageProductSchema,
+    ProductNameSchema,
     SingleMessageSchema,
     UpdateProductSchema,
     format_product_response,
@@ -27,6 +27,8 @@ TAG_PRODUCTS = Tag(name="Product", description="Product data control routes.")
 )
 def add_product(form: AddProductSchema):
     """Add a new product to the product table."""
+    log.add_message("Add_procuct route accessed")
+
     name = unquote(unquote(form.name)).strip().title()
     price = round(form.price, 2)
     supplier = unquote(unquote(form.supplier)).strip().title()
@@ -39,8 +41,12 @@ def add_product(form: AddProductSchema):
             column=Product.name, filter_select={Product.name: name}
         )
 
+        log.add_message(f"Checking if the {name} product exists")
+
         if len(registered_product) > 0:
             raise Exception("Product already registered")
+
+        log.add_message(f"{name} product does not exist")
 
         new_product = Product(
             name=name,
@@ -53,9 +59,26 @@ def add_product(form: AddProductSchema):
         formatted_response = format_product_response(product=new_product)
         database.insert_data_table(new_product)
 
-        return {"message": "Added Product", "product": formatted_response}, 200
+        log.add_message(f"{name} added")
+
+        return_data = {
+            "message": "Added Product",
+            "product": formatted_response
+        }
+
+        log.add_message(f"Api response: {return_data}")
+        log.add_message("Api Status: 200")
+        log.add_message("")
+
+        return return_data, 200
     except Exception as error:
-        return {"message": f"Error: {error}"}, 400
+        return_data = {"message": f"Error: {error}"}
+
+        log.add_message(f"Api response: {return_data}")
+        log.add_message("Api Status: 400")
+        log.add_message("")
+
+        return return_data, 400
 
 
 @app.put(
@@ -68,6 +91,8 @@ def add_product(form: AddProductSchema):
 )
 def update_stock(form: UpdateProductSchema):
     """Updates the available stock of the specified product."""
+    log.add_message("Update_stock route accessed")
+
     name = unquote(unquote(form.name)).strip().title()
     new_stock = form.new_stock
 
@@ -76,8 +101,12 @@ def update_stock(form: UpdateProductSchema):
             column=Product.name, filter_select={Product.name: name}
         )
 
+        log.add_message(f"Checking if the {name} product exists")
+
         if len(registered_product) == 0:
             raise Exception("The product does not exist")
+
+        log.add_message(f"{name} product exists")
 
         old_stock = database.select_value_table_parameter(
             column=Product.available_stock, filter_select={Product.name: name}
@@ -92,9 +121,26 @@ def update_stock(form: UpdateProductSchema):
             new_data={Product.available_stock: new_stock},
         )
 
-        return {"message": "Updated stock", "product": formatted_response}, 200
+        log.add_message(f"Updated {name} stock to {new_stock}")
+
+        return_data = {
+            "message": "Updated stock",
+            "product": formatted_response
+        }
+
+        log.add_message(f"Api response: {return_data}")
+        log.add_message("Api Status: 200")
+        log.add_message("")
+
+        return return_data, 200
     except Exception as error:
-        return {"message": f"Error: {error}"}, 400
+        return_data = {"message": f"Error: {error}"}
+
+        log.add_message(f"Api response: {return_data}")
+        log.add_message("Api Status: 400")
+        log.add_message("")
+
+        return return_data, 400
 
 
 @app.delete(
@@ -107,6 +153,8 @@ def update_stock(form: UpdateProductSchema):
 )
 def delete_product(form: ProductNameSchema):
     """Delete a product from the product table."""
+    log.add_message("Delete_product route accessed")
+
     name = unquote(unquote(form.name)).strip().title()
 
     try:
@@ -114,26 +162,48 @@ def delete_product(form: ProductNameSchema):
             column=Product.name, filter_select={Product.name: name}
         )
 
+        log.add_message(f"Checking if the {name} product exists")
+
         if len(registered_product) == 0:
             raise Exception("The product does not exist")
+
+        log.add_message(f"{name} product exists")
 
         product_sold = database.select_value_table_parameter(
             column=Sales.name, filter_select={Sales.name: name}
         )
+
+        log.add_message("Checking if the product has already been sold")
 
         if len(product_sold) > 0:
             raise Exception(
                 f"{name} has already been sold, it's not possible to delete it"
             )
 
+        log.add_message("The product has not yet been sold")
+
         database.delete_data_table(
             table=Product,
             filter_delete={Product.name: name},
         )
 
-        return {"message": "Product deleted", "name": name}, 200
+        log.add_message(f"{name} deleted")
+
+        return_data = {"message": "Product deleted", "name": name}
+
+        log.add_message(f"Api response: {return_data}")
+        log.add_message("Api Status: 200")
+        log.add_message("")
+
+        return return_data, 200
     except Exception as error:
-        return {"message": f"Error: {error}"}, 400
+        return_data = {"message": f"Error: {error}"}
+
+        log.add_message(f"Api response: {return_data}")
+        log.add_message("Api Status: 400")
+        log.add_message("")
+
+        return return_data, 400
 
 
 @app.get(
@@ -146,6 +216,8 @@ def delete_product(form: ProductNameSchema):
 )
 def get_product(query: ProductNameSchema):
     """Method to obtain data of registered products."""
+    log.add_message("Get_product route accessed")
+
     name = unquote(unquote(query.name)).strip().title()
 
     try:
@@ -153,16 +225,37 @@ def get_product(query: ProductNameSchema):
             column=Product.name, filter_select={Product.name: name}
         )
 
+        log.add_message(f"Checking if the {name} product exists")
+
         if len(registered_product) == 0:
             raise Exception("The product does not exist")
+
+        log.add_message(f"{name} product exists")
 
         product_data = database.select_data_table(
             table=Product,
             filter_select={Product.name: name},
         )[0]
 
+        log.add_message("Products listed")
+
         formatted_response = format_product_response(product_data)
 
-        return {"message": "Product all data", "product": formatted_response}, 200
+        return_data = {
+            "message": "Product all data",
+            "product": formatted_response
+        }
+
+        log.add_message(f"Api response: {return_data}")
+        log.add_message("Api Status: 200")
+        log.add_message("")
+
+        return return_data, 200
     except Exception as error:
-        return {"message": f"Error: {error}"}, 400
+        return_data = {"message": f"Error: {error}"}
+
+        log.add_message(f"Api response: {return_data}")
+        log.add_message("Api Status: 400")
+        log.add_message("")
+
+        return return_data, 400
