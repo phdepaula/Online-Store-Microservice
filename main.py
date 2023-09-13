bc0fb25775ea3ce2603989afa83e8dce30580c54
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-09-13 10:06:01 -0300

    feat: Adding log

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import resources.documentation
 import resources.products
 import resources.sales
-
-from app import flask_settings, database, log
-
+from app import database, flask_settings, log
 
 if __name__ == "__main__":
-    database.setup_database_environment()
-    flask_settings.run_aplication()
     log.start_log()
+
+    log.add_message("Starting the database environment")
+    database.setup_database_environment()
+
+    log.add_message("Starting Flask Settings")
+    log.add_message(" ")
+    flask_settings.run_aplication()
